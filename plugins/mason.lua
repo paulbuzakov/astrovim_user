837559284fc934fabdac80ae49aa1b501b833850
Paul Buzakov <buzakovpa@gmail.com>
2023-08-17 16:09:29 +0500

    feat: Init my settings
    modified:   init.lua
    modified:   plugins/core.lua
    modified:   plugins/mason.lua
    modified:   plugins/null-ls.lua
    modified:   plugins/treesitter.lua
    modified:   plugins/user.lua

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -7,7 +7,10 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "lua_ls",
+        "csharp_ls",
+        "cssls",
+        "cssmodules_ls",
+        "tsserver",
       })
     end,
   },
@@ -18,8 +21,8 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "prettier",
-        -- "stylua",
+        "prettierd",
+        "stylua",
       })
     end,
   },
@@ -29,7 +32,9 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "python",
+        "chrome",
+        "firefox",
+        "coreclr",
       })
     end,
   },
--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -2,15 +2,26 @@ return {
   "jose-elias-alvarez/null-ls.nvim",
   opts = function(_, config)
     -- config variable is the default configuration table for the setup function call
-    -- local null_ls = require "null-ls"
+    local null_ls = require "null-ls"
 
     -- Check supported formatters and linters
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
     config.sources = {
+      -- Set a code action
+      null_ls.builtins.code_actions.eslint_d,
+      null_ls.builtins.code_actions.refactoring,
+
+      -- Set a completion
+      null_ls.builtins.completion.vsnip,
+
+      -- Set a diagnostics
+      null_ls.builtins.diagnostics.eslint_d,
+      null_ls.builtins.diagnostics.tsc,
+
       -- Set a formatter
-      -- null_ls.builtins.formatting.stylua,
-      -- null_ls.builtins.formatting.prettier,
+      null_ls.builtins.formatting.stylua,
+      null_ls.builtins.formatting.prettierd,
     }
     return config -- return final config table
   end,
